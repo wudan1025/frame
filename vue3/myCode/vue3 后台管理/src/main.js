3a1aaf0021ecb74e9804b6291d6aa5a4dbba518d
wudan <xes@wudandeMacBook-Pro.local>
2022-04-14 18:50:48 +0800

    add

--- vue3/myCode/vue3 后台管理/src/main.js
+++ vue3/myCode/vue3 后台管理/src/main.js
@@ -10,9 +10,12 @@ import store from './store';
 import installElementPlus from './plugins/element';
 // 导入 svgIcon
 import installIcons from '@/icons';
+// i18n （PS：导入放到 APP.vue 导入之前，因为后面我们会在 app.vue 中使用国际化内容）
+import i18n from '@/i18n';
 import './styles/index.scss';
 
 const app = createApp(App);
 installElementPlus(app);
 installIcons(app);
+app.use(i18n);
 app.use(store).use(router).mount('#app');
