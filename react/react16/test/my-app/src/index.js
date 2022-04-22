7d045123ca006f08786dea8a8806d80898bab607
wudan <xes@wudandeMacBook-Pro.local>
2022-04-22 15:11:13 +0800

    ad

--- react/react16/test/my-app/src/index.js
+++ react/react16/test/my-app/src/index.js
@@ -8,7 +8,8 @@ import React from 'react';
 // client.js / index.js 版本差异
 import ReactDOM, { render } from 'react-dom';
 import './index.css';
-import event from './event.js';
+import compoent from './compoent';
+// import event from './event.js';
 // import state from './state';
 // import props from './props';
 // import test from './test';
